31763b0dcd431add2a4a853a90afa97fad57c980
Bledhard <1akaant1@gmail.com>
2017-08-25 14:54:47 +0300

    add 'hello-world' unit test

---
+++ sad/Tests/CustomerControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AndersenTrainee1.Controllers;
+using AndersenTrainee1.EntityFramework.Entities;
+using System.Collections.Generic;
+
+namespace AndersenTrainee1.Tests.Controllers
+{
+    [TestClass]
+    public class CustomerControllerTest
+    {
+        [TestMethod]
+        public void GetCustomerNotNull()
+        {
+            var controller = new CustomerController();
+
+            var customer = controller.Get(1) as Customer;
+
+            Assert.IsNotNull(customer);
+        }
+
+        [TestMethod]
+        public void GetListCustomerNotNull()
+        {
+            var controller = new CustomerController();
+
+            var customers = controller.Get() as List<Customer>;
+
+            Assert.IsNotNull(customers);
+        }
+    }
+}
